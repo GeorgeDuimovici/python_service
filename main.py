d5d5eac9c43923e63a7e52de4f8bcf1e0bcccceb
georged <georgeduimovici@Georges-MacBook-Pro.local>
2025-07-01 20:38:39 +0300

    curs2DevOps

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("yolo")
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <location>")
+        sys.exit(1)
+    location = sys.argv[1]
+    url = f"https://wttr.in/{location}"
+    response = requests.get(url)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
