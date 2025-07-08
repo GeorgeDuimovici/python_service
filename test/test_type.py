16ada4309c320ad720d880e44c92f813b5da98d6
georged <georgeduimovici@Georges-MacBook-Pro.local>
2025-07-08 20:36:16 +0300

    testing3

--- test/test_type.py
+++ test/test_type.py
@@ -2,11 +2,14 @@ import pytest
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from type_test import add
+
 
 def test_add():
     assert add(5, 10) == 15
     assert add("hello", " world") == "hello world"
     assert add(3.5, 2.5) == 6.0
-    assert add("cat", 1) == "TypeError: unsupported operand type(s) for +: 'str' and 'int'"
+    assert (
+        add("cat", 1) == "TypeError: unsupported operand type(s) for +: 'str' and 'int'"
+    )
--- type_test.py
+++ type_test.py
@@ -1,8 +1,6 @@
-
 def add(a, b):
     if isinstance(a, (str, int, float) or isinstance(b, str, int, float)):
         return a + b
-
 
 
 # a = 5
@@ -12,11 +10,11 @@ def add(a, b):
 # def add(a, b):
 #     """Add two numbers."""
 #     print(a + b)
-    
+
 # if __name__ == "__main__":
 #     a = "cat"
 #     b = 10
-    
+
 #     add(a, b)
 
 ### Tema de a scrie functia in pythonic si folosind pydantic
