897e404a58a2b27e271f65c67a285c650ef7428d
georged <georgeduimovici@Georges-MacBook-Pro.local>
2025-07-08 20:07:57 +0300

    added tests incomplete

---
+++ type_test.py
@@ -0,0 +1,22 @@
+
+def add(a, b):
+    if isinstance(a, (str, int, float) or isinstance(b, str, int, float)):
+        return a + b
+
+
+
+# a = 5
+# b = 10
+
+
+# def add(a, b):
+#     """Add two numbers."""
+#     print(a + b)
+    
+# if __name__ == "__main__":
+#     a = "cat"
+#     b = 10
+    
+#     add(a, b)
+
+### Tema de a scrie functia in pythonic si folosind pydantic
